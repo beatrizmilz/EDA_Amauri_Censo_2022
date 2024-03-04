8d198c7550d092e570495209074b1d8ce68ffb39
Beatriz Milz <milz.bea@gmail.com>
2024-03-04 18:11:31 -0300

    Initial commit

---
+++ municipios_interesse.R
@@ -0,0 +1,36 @@
+muni_prioritarios_para_tarcisio <-
+  c(
+    "São Paulo",
+    "Guarulhos",
+    "São Bernardo do Campo",
+    "Santo André",
+    "Santos",
+    "Osasco",
+    "São José dos Campos",
+    "Diadema",
+    "Barueri",
+    "Praia Grande",
+    "Franca"
+  )
+
+muni_intermediarios <-
+  c(
+    "Guarujá",
+    "Suzano",
+    "São Vicente",
+    "Carapicuíba",
+    "Taubaté",
+    "Presidente Prudente",
+    "Itaquaquecetuba",
+    "Taboão da Serra",
+    "Cotia",
+    "Hortolândia"
+  )
+
+muni_sedes_regionais_sabesp <-
+  c("Caraguatatuba",
+    "Botucatu",
+    "Itapetininga",
+    "Itatiba",
+    "Lins",
+    "Registro")
